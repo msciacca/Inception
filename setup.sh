63124a0d39a3c45d503d824c19f0677fa081e18d
msciacca <msciacca@student.42roma.it>
2023-07-19 19:46:21 +0200

    Auto accept apt

--- setup.sh
+++ setup.sh
@@ -38,14 +38,10 @@ echo -e $PURPLE "Installing docker dependencies..."
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 echo -e $GREEN "Done."
 
-echo -e $PURPLE "Downloading and adding docker gpg..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo -e $GREEN "Done."
-
 echo -e $PURPLE "Installing docker..."
-sudo apt-get remove docker.io docker-doc docker-compose podman-docker containerd runc
+sudo apt-get remove -y docker.io docker-doc docker-compose podman-docker containerd runc
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg --insecure | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -54,7 +50,7 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo -e $GREEN "Done."
 
 echo -e $GREEN "Config completed. " $RED "Rebooting in 5 seconds..." $BLANK
